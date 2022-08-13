3da210039d723a48944fd35eab2f5f5a7347e9d7
Atsedewoyn <atsede.molla27@gmail.com>
2022-08-13 11:59:32 +0300

    first commiting

--- components/Desktop/Nav.vue
+++ components/Desktop/Nav.vue
@@ -56,6 +56,7 @@ export default {
         {title:'Contact',  route:'contact'},
         
       ]
+    
      }
   },
   methods: {
--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ["@sweetalert2/theme-material-ui", "~/assets/main.css"],
-
+  
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ["~/plugins/cart.js"],
 
